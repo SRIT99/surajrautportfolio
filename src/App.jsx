32048f786b370b1899c3c85bb1f1191044b70949
Suraj <surajworkspace12@gmail.com>
2026-07-26 16:49:00 +0545

    improved routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import { Route, Routes } from 'react-router-dom';
 import './index.css';
-
+import Home from './pages/Home'
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Contact from './pages/Contact';
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/skills' element={<Skills />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/experience' element={<Experience />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+  )
 }
 
 export default App;
